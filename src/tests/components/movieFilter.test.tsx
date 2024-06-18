43b047e756dbc617276bcc4ac1e92c190e660916
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-06-18 07:55:16 -0300

    test: should render selectButton correctly with selected

--- src/tests/components/movieFilter.test.tsx
+++ src/tests/components/movieFilter.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { MovieFilter } from "../../components";
 import { useNavigate } from "react-router-dom";
 
@@ -6,12 +6,43 @@ jest.mock("react-router-dom")
 const mockUseNavigate = jest.mocked(useNavigate);
 
 describe("MovieFilter", () => {
-    mockUseNavigate.mockReturnValue(jest.fn());
 
+    afterEach(() => {
+        jest.clearAllMocks();
+    })
+    
     it("should render correctly", () => {
+        mockUseNavigate.mockReturnValue(jest.fn());
+
         render(<MovieFilter genres={[]} genresSelecteds={[]}/>);
 
         const title = screen.getByRole("heading");
         expect(title).toHaveTextContent("Milhões de filmes, séries e pessoas para descobrir. Explore já.");
     });
+
+    it("should render genreLList correctly", () => {
+        mockUseNavigate.mockReturnValue(jest.fn());
+
+        render(<MovieFilter genres={[{ id: 1, name: "test" }, { id: 2, name: "test2" }, { id: 3, name: "test3" }]} genresSelecteds={[]}/>);
+
+        const selectButton = screen.getAllByRole("button");
+        expect(selectButton).toHaveLength(3);
+    });
+
+    it("should render selectButton correctly with selected", () => {
+        mockUseNavigate.mockReturnValue(jest.fn());
+
+        const { rerender } = render(
+            <MovieFilter genres={[{ id: 1, name: "test" }, { id: 2, name: "test2" }, { id: 3, name: "test3" }]} genresSelecteds={[]} />
+        );
+    
+        const selectButton = screen.getAllByRole("button");
+        fireEvent.click(selectButton[0]);
+       
+        rerender(
+            <MovieFilter genres={[{ id: 1, name: "test" }, { id: 2, name: "test2" }, { id: 3, name: "test3" }]} genresSelecteds={[1]} />
+        );
+
+        expect(selectButton[0]).toHaveClass("selected");
+    })
 })
