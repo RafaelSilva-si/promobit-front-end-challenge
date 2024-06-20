8c24bc0e96d8460d871aa8a12c7eb89c67b1c539
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-06-20 08:24:16 -0300

    fix: adjust movieCard components

--- src/components/shared/movieCard/index.tsx
+++ src/components/shared/movieCard/index.tsx
@@ -7,14 +7,16 @@ interface Props {
 
 export const MovieCard: React.FC<Props> = ({ movie }) => {
   return (
-    <Link to={`/movie-details/${movie.id}`} style={{ textDecoration: "none" }}>
-      <img
-        src={movie.poster_path}
-        alt="Poster do filme"
-        style={{ width: "100%", height: "260px" }}
-      />
-      <h2>{movie.title}</h2>
-      <h3>{movie.release_date}</h3>
-    </Link>
+    <li style={{ listStyle: "none" }}>
+      <Link to={`/movie-details/${movie.id}`} style={{ textDecoration: "none" }}>
+        <img
+          src={movie.poster_path}
+          alt="Poster do filme"
+          style={{ width: "100%", height: "260px" }}
+        />
+        <h2>{movie.title}</h2>
+        <h3>{movie.release_date}</h3>
+      </Link>
+    </li>
   );
 };
